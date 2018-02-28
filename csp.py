e389b7c2f75d3e6a0440e19073484aca4e555f3c
jjups96 <jjups96@gmail.com>
2018-02-27 21:09:08 -0700

    AC-3
    Se agrego AC3: Se agrego una linea al tipo1

--- csp.py
+++ csp.py
@@ -229,7 +229,22 @@ def consistencia(gr, ap, xi, vi, tipo):
         #    Implementar el algoritmo de AC3
         #    y print()robarlo con las n-reinas
         # ================================================
-        raise NotImplementedError("AC-3  a implementar")
+        pendientes = deque([(xj, xi) for xj in gr.vecinos[xi] if xj not in ap])
+        while pendientes:
+            xa, xb = pendientes.popleft()
+            temp = reduceAC3(xa, xb, gr)
+
+            if temp:
+                if not gr.dominio[xa]:
+                    gr.dominio[xa] = temp
+                    for v in dom_red.keys():
+                        gr.dominio[v] = gr.dominio[v].union(dom_red[v])
+                    return None
+                else:
+                    if xa not in dom_red:
+                        dom_red[xa] = set({})
+                    dom_red[xa] = dom_red[xa].union(temp)
+                    pendientes.extend([(z, xa) for z in gr.vecinos[xa] if z != xb])
 
     return dom_red
 
